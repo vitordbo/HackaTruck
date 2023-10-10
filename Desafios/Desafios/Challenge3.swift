68f37e2689a49ce77458583c67b143e16ccfb82c
Vitor Duarte <65680799+vitordbo@users.noreply.github.com>
2023-10-10 19:27:26 -0300

    Improvements on challenge 3

--- Desafios/Desafios/Challenge3.swift
+++ Desafios/Desafios/Challenge3.swift
@@ -8,35 +8,36 @@
 import SwiftUI
 
 struct Challenge3: View {
-    @State private var name: String = "Vitor"
+    @State private var name: String = ""
     @State private var showingAlert = false
 
     var body: some View {
-        VStack{
-            Text("Bem-vindo, " + name).font(.system(size: 30)).frame(alignment: .center)
-                TextField(
-                  "Digite seu nome",
-                  text: $name
-                ).frame(alignment: .center).multilineTextAlignment(.center).frame(alignment: .top)
-            Spacer()
             ZStack{
-                Image("iamge1").resizable().scaleEffect(1.0).opacity(0.2).padding(EdgeInsets(top: 10, leading: 10, bottom: -60, trailing: 10))
-                Image("logo").resizable().scaleEffect(0.3).frame(width: 600)
-                Image("truck").resizable().frame(width: 600, height: 200).scaleEffect(0.5).padding(EdgeInsets(top: 320, leading: 10, bottom: 10, trailing: 10))
-            }
-            Button("Enviar") {
+                Image("iamge1").resizable().scaleEffect(1.2).opacity(0.2).padding(EdgeInsets(top: 10, leading: 10, bottom: -60, trailing: 10))
+                Image("logo").resizable().scaleEffect(0.3).frame(width: 700, height: 500).shadow(radius: 10)
+                Image("truck").resizable().frame(width: 500, height: 200).scaleEffect(0.5).padding(EdgeInsets(top: 300, leading: 10, bottom: 10, trailing: 10))
+                
+                VStack{
+                    Text("Bem-vindo, " + name).font(.largeTitle).frame(alignment: .center)
+                    TextField(
+                        "Digite seu nome",
+                        text: $name
+                    ).frame(alignment: .center).multilineTextAlignment(.center).frame(alignment: .top)
+                    Spacer()
+                    Button("Entrar") {
                         showingAlert = true
                     }
                     .alert(isPresented:$showingAlert) {
                         Alert(
-                            title: Text("Parabéns, voce completou o desafio de hoje"),
-                            primaryButton:.default (Text("OK")) {
-                                print("Deleting...")
-                            },
+                            title: Text("Alerta"),
+                            message: Text("Parabéns, voce completou o desafio de hoje"),
+                            primaryButton:.default (Text("OK")),
                             secondaryButton: .cancel()
                         )
+                        
                     }
-        }
+                }
+            }
     }
 }
 
